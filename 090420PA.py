af7696041c74b46e3cfb1fa6905a00ec1f3c3d4e
dingaotearoa <51272259+dingaotearoa@users.noreply.github.com>
2020-09-04 17:43:54 +1200

    Add files via upload

---
+++ 090420PA.py
@@ -0,0 +1,83 @@
+# Regularization techniques
+
+# Instantiate the network
+model = Net()
+
+# Instantiate the cross-entropy loss
+criterion = nn.CrossEntropyLoss()
+
+# Instantiate the Adam optimizer
+optimizer = optim.Adam(model.parameters(), lr=3e-4, weight_decay=0.001
+                       
+class Net(nn.Module):
+    def __init__(self):
+
+        # Define all the parameters of the net
+        self.classifier = nn.Sequential(
+            nn.Linear(28*28, 200),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(200, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 10))
+
+    def forward(self, x):
+
+        # Do the forward pass
+        return self.classifier(x)
+    
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        # Implement the sequential module for feature extraction
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(2, 2), nn.ReLU(inplace=True), nn.BatchNorm2d(10),
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(2, 2), nn.ReLU(inplace=True), nn.BatchNorm2d(20))
+        
+        # Implement the fully connected layer for classification
+        self.fc = nn.Linear(in_features=(28*0.25)*(28*0.25)*20, out_features=10)
+
+# Transfer learning
+
+# Create a model using
+model = Net()
+
+# Load the parameters from the old model
+model.load_state_dict(torch.load('my_net.pth'))
+
+# Change the number of out channels
+model.fc = nn.Linear(7 * 7 * 512, 26)
+
+# Train and evaluate the model
+model.train()
+train_net(model, optimizer, criterion)
+print("Accuracy of the net is: " + str(model.eval()))
+
+# Create a model using
+model = Net()
+
+# Load the parameters from the old model
+model.load_state_dict(torch.load('my_net.pth'))
+
+# Change the number of out channels
+model.fc = nn.Linear(7 * 7 * 512, 26)
+
+# Train and evaluate the model
+model.train()
+train_net(model, optimizer, criterion)
+
+# Import the module
+import torchvision
+
+# Download resnet18
+model = torchvision.models.resnet18(pretrained=True)
+
+# Freeze all the layers bar the last one
+for param in model.parameters():
+    param.requires_grad = False
+
+# Change the number of output units
+model.fc = nn.Linear(512, 7)
